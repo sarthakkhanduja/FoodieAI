fe126be9fb23f62565da8640ea0c8de579d416b0
sarthakkhanduja <sarthakkhanduja98@gmail.com>
2024-06-16 02:09:37 -0400

    Fixed the frontend styling and added components to check the preferences

--- server.py
+++ server.py
@@ -14,28 +14,36 @@ os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 # Prompt Template
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a professional chef/nutritionist. Your job is to provide the best possible recipe for the ingredients available with the user (separated by a comma). You need to adjust the recipe according to the nutrition preference given by the user. If not given, you can assume that the user is okay to eat however calorie intensive or extensive the recipe is. You can assume that the user has spices available. Make sure you mention the nutritional value of the recipe with how many people it would serve."
-                   "that the user might have"),
-        ("user", "List of Ingredients: {question}. Nutritional value preference: {nutri-value}")
+        ("system", "You are a professional chef/nutritionist. Your job is to provide the best possible recipe for the ingredients available with the user (separated by a comma). You need to adjust the recipe according to the cuisine preference given by the user. You also need to make sure that if the user has given any nutritional preference, then the recipe needs to adhered to it. If the nutritional preference is not given, you can assume that the user is okay to eat however calorie intensive or extensive the recipe is. You can assume that the user has spices available. Make sure you mention the nutritional value of the recipe with how many people it would serve. You don't have to use all the available ingredients. It's not mandatory, but if needed, go for it."),
+        ("user", "List of Ingredients: {question}. Nutritional value preference: {nutri_value}. Cuisine: {cuisine}")
     ]
 )
-
-# filled_prompt = prompt.format(question="What is the weather going to be if it rained yesterday, but has been sunny since morning, in the summer season?")
-# print(filled_prompt)
 
 # StreamLit Framework
 
 st.title('FoodieAI')
-ingredients = st.text_input("Please enter the list of ingredients separated by a comma")
-nutri_value = st.text_input("Please enter any specific type of nutritional value you would prefer in your recipe (such as, protein-rich, less-sugar, etc.)")
+ingredients = st.text_input("Please enter the list of ingredients separated by a comma", key="ingredients")
+nutri_value = st.text_input("Please enter any specific type of nutritional value you would prefer in your recipe (such as, protein-rich, less-sugar, etc.)", key="nutri_value")
+cuisine = st.text_input("Do you have any cuisine preference?", key="cuisine")
 
-
-# Ollama LLM
+# LLM
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
 output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
 
-if ingredients and nutri_value:
-    st.write(chain.invoke({'question' : ingredients, 'nutri-value': nutri_value}))
-elif ingredients:
-    st.write(chain.invoke({'question' : ingredients}))
+# Button to trigger the AI
+if st.button("Get Recipe"):
+    # Add a divider
+    st.markdown("<hr>", unsafe_allow_html=True)
+    if ingredients:
+        query = {
+            'question': ingredients,
+            'nutri_value': nutri_value if nutri_value else "No preference",
+            'cuisine': cuisine if cuisine else "No preference"
+        }
+        response = chain.invoke(query)
+        st.write(response)
+    else:
+        st.write("Please enter the list of ingredients to get a recipe.")
+
+
